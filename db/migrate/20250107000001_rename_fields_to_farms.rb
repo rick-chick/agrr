fda003d4ba49c6ab0675663f1347b45424f27595
wor1d <wor1d@example.com>
2025-10-15 22:13:17 +0900

    Remove problematic migration files that cause errors
    Remove old migration files that were causing errors during db:migrate:
    - Files attempting to rename/create fields/farms tables that don't exist
    - Files creating/dropping regions and farm_sizes tables
    - Files related to free_crop_plans table
    
    These migrations were inconsistent with schema.rb and causing:
    - 'no such table: fields' error when trying to rename
    - 'no such table: farms' error when adding columns
    - Database reset failures
    
    Now use schema.rb directly for database setup (db:schema:load)

--- db/migrate/20250107000001_rename_fields_to_farms.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class RenameFieldsToFarms < ActiveRecord::Migration[8.0]
-  def change
-    rename_table :fields, :farms
-  end
-end
-
-
--- db/migrate/20251012072243_drop_farm_sizes_table.rb
+++
@@ -1,4 +0,0 @@
-class DropFarmSizesTable < ActiveRecord::Migration[8.0]
-  def change
-  end
-end
