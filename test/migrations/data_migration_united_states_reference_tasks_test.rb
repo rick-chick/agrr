86ffd7c96895df8b2b043280a9cdcc40da2a8779
wor1d <wor1d@example.com>
2025-11-07 23:57:46 +0900

    Revert "Add US reference task migration"
    This reverts commit 33b8744af40ac2f9d0bd12d4dd46c9a0b756d65f.

--- test/migrations/data_migration_united_states_reference_tasks_test.rb
+++ test/migrations/data_migration_united_states_reference_tasks_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 require Rails.root.join('db/migrate/20251107193000_data_migration_united_states_reference_tasks')
 
 class DataMigrationUnitedStatesReferenceTasksTest < ActiveSupport::TestCase
+  EXPECTED_TASKS = DataMigrationUnitedStatesReferenceTasks::TASK_DEFINITIONS.transform_values do |definition|
+    definition.merge(crops: definition[:crops].sort)
+  end.freeze
+
   def setup
     AgriculturalTaskCrop.delete_all
     AgriculturalTask.delete_all
@@ -17,7 +21,7 @@ class DataMigrationUnitedStatesReferenceTasksTest < ActiveSupport::TestCase
   def test_up_creates_reference_tasks_and_assigns_crops
     @migration.up
 
-    expected_tasks.each do |name, attributes|
+    EXPECTED_TASKS.each do |name, attributes|
       task = AgriculturalTask.find_by(name: name, region: 'us', is_reference: true)
       assert task, "Expected task '#{name}' to be created"
 
@@ -28,7 +32,6 @@ class DataMigrationUnitedStatesReferenceTasksTest < ActiveSupport::TestCase
       assert_equal attributes[:skill_level], task.skill_level
       assert_nil task.user_id
       assert task.is_reference
-      assert_equal 'us', task.region
 
       assert_equal attributes[:crops], task.crops.pluck(:name).sort
     end
@@ -38,7 +41,7 @@ class DataMigrationUnitedStatesReferenceTasksTest < ActiveSupport::TestCase
     @migration.up
     @migration.down
 
-    expected_tasks.keys.each do |name|
+    EXPECTED_TASKS.keys.each do |name|
       assert_nil AgriculturalTask.find_by(name: name, region: 'us', is_reference: true)
     end
 
@@ -48,12 +51,8 @@ class DataMigrationUnitedStatesReferenceTasksTest < ActiveSupport::TestCase
   private
 
   def reference_crop_names
-    expected_tasks.values.flat_map { |attrs| attrs[:crops] }.uniq
-  end
-
-  def expected_tasks
-    @expected_tasks ||= DataMigrationUnitedStatesReferenceTasks::TASK_DEFINITIONS.transform_values do |definition|
-      definition.merge(crops: definition[:crops].sort)
-    end.freeze
+    EXPECTED_TASKS.values.flat_map { |attrs| attrs[:crops] }.uniq
   end
 end
+
+
