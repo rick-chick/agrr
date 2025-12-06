0741664413344f43dd7d3ac246e396af5561d6ce
wor1d <wor1d@example.com>
2025-12-06 09:19:56 +0900

    feat: Add unique index on solid_cable_messages id column
    - Introduced a migration to add a unique index on the id column of the solid_cable_messages table.
    - This change is necessary for the correct operation of the insert_all method.

---
+++ db/cable_migrate/20251205000001_add_unique_index_on_solid_cable_messages_id.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddUniqueIndexOnSolidCableMessagesId < ActiveRecord::Migration[8.0]
+  def change
+    # solid_cable_messagesテーブルにidカラムの一意インデックスを追加
+    # insert_allが正しく動作するために必要
+    unless index_exists?(:solid_cable_messages, :id)
+      add_index :solid_cable_messages, :id, unique: true
+    end
+  end
+end
+
